6e8cca0f4456108a4ba8d280a12ac8d6e28c541b
Sergey A. Kozlov <65397484+Sergey371@users.noreply.github.com>
2022-05-23 20:02:12 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@ from decimal import Decimal
 
 print('Hello, World!')
 
-netto = Decimal("2312")
+D = Decimal("2312")
 
 print("Нетто         : " + str(D))
 print("Нетто в 10 шт.: " + str(D(netto * 10)))
